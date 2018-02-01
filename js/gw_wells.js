9494dbc2123621bde0d3a00bd3e1fb7881fe18f7
IanLaingBCGov <Ian.Laing@gov.bc.ca>
2018-02-01 10:52:33 -0800

    Wells WMS request
    Wells WMS request

---
+++ js/gw_wells.js
@@ -0,0 +1,135 @@
+﻿/* -----------------------------------------------------------------------------------
+   Groundwater Wells
+   Developed by GeoBC
+   (c) 2018 GeoBC | http://www.geobc.gov.bc.ca
+----------------------------------------------------------------------------------- */
+
+$.urlParam = function(name) {
+  var results = new RegExp('[\\?&]' + name + '=([^&#]*)')
+      .exec(window.location.href);
+  if (!results) {
+    return 0;
+  }
+  return results[1] || 0;
+};
+
+$( document ).ready(function() {
+
+  var geomarkId = $.urlParam('geomarkId');
+  var map = L.map('map', {
+    minZoom: 1
+  }).setView([49.6, -119.5], 9);
+
+/*-----BASE MAPS-----*/
+
+  var provRoadsWM = new L.tileLayer.wms("http://maps.gov.bc.ca/arcserver/services/province/roads_wm/MapServer/WMSServer", {
+    	 layers: '0',
+        format: 'image/png',
+        transparent: false,
+        attribution: "© 2013-2018 GeoBC, DataBC, The Province of British Columbia"
+	}).addTo(map);
+
+  var provWebMercatorCache = new L.tileLayer.wms("http://maps.gov.bc.ca/arcserver/services/Province/web_mercator_cache/MapServer/WMSServer", {
+        layers: '0',
+        format: 'image/png',
+        transparent: false,
+        attribution: "© 2013-2018 GeoBC, DataBC, The Province of British Columbia"
+	});
+
+  /*
+  var SPOTimagery = new L.tileLayer.wms("http://geo_bc:rzVBvz7a@geocloud.blackbridge.com/service?", {
+      //layers: '0',
+      //format: 'image/png',
+      //transparent: true,
+      attribution: "© 2017 geomatics.planet.com",
+      crs:crs84
+  });
+  */
+
+/*-----OVERLAYS-----*/
+
+  var crs84 = new L.Proj.CRS('CRS:84', '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs');
+
+  /*-----OKANAGAN LAKE IMAGERY-----*/
+  /*
+  var OKimagery2013 = new L.tileLayer.wms("http://openmaps.gov.bc.ca/imagex/ecw_wms.dll?", {
+        layers: 'REGIONAL_MOSAICS_BC_OKANAGAN_LAKE_XC200MM_2013_BCALB',
+        format: 'image/png',
+        transparent: true,
+        maxZoom: 21,
+        attribution: "© 2013-2018 GeoBC, DataBC, The Province of British Columbia"
+  });
+
+  var OKimagery2018 = new L.tileLayer.wms("http://test.openmaps.gov.bc.ca/lzt/ows?", {
+        layers: 'o17',
+        format: 'image/png',
+        transparent: true,
+        maxZoom: 21,
+        attribution: "© 2013-2018 GeoBC, DataBC, The Province of British Columbia"
+  }).addTo(map);
+  */
+
+  /*-----AQUIFERS POLY-----*/
+  var aquiferPolys = new L.tileLayer.wms("https://openmaps.gov.bc.ca/geo/pub/WHSE_WATER_MANAGEMENT.GW_AQUIFERS_CLASSIFICATION_SVW/ows", {
+        layers: 'pub:WHSE_WATER_MANAGEMENT.GW_AQUIFERS_CLASSIFICATION_SVW',
+        format: 'image/png',
+        transparent: true,
+        attribution: "© 2013-2016 GeoBC, DataBC, The Province of British Columbia",
+        crs:crs84,
+        styles: 'Aquifers_BC_Outlined'
+  });
+
+  /*-----GROUNDWATER WELLS-----*/
+
+  var gwWells = new L.tileLayer.wms("https://openmaps.gov.bc.ca/geo/pub/WHSE_WATER_MANAGEMENT.GW_AQUIFERS_CLASSIFICATION_SVW/ows?SERVICE=WMS&", {
+        layers: 'pub:WHSE_WATER_MANAGEMENT.GW_WATER_WELLS_WRBC_SVW',
+        format: 'image/png',
+        version: '1.3.0',
+        transparent: false,
+        attribution: "© 2013-2018 GeoBC, DataBC, The Province of British Columbia",
+        crs:crs84,
+        styles: 'Provincial_Groundwater_Observation_Wells_Active'
+  }).addTo(map);
+
+
+  /*-----POINTS OF DIVERSION-----*/
+  /*
+  var pointsOfDiversion = new L.tileLayer.wms("https://openmaps.gov.bc.ca/geo/pub/WHSE_WATER_MANAGEMENT.GW_AQUIFERS_CLASSIFICATION_SVW/ows", {
+        layers: 'pub:WHSE_WATER_MANAGEMENT.GW_WATER_WELLS_WRBC_SVW',
+        format: 'image/png',
+        transparent: true,
+        attribution: "© 2013-2016 GeoBC, DataBC, The Province of British Columbia",
+        crs:crs84,
+        styles: 'Points_of_Diversion'
+  }).addTo(map);
+  */
+
+
+  /*-----Layer Control-----*/
+  var layerControl = L.control.layers(
+    {
+    'Roads Base Map': provRoadsWM,
+    'Terrain Base Map': provWebMercatorCache
+    //'SPOT Imagery': SPOTimagery
+    },
+    {
+    'Groundwater Wells (Scale Dependent)': gwWells,
+    'Aquifers (Scale Dependent)': aquiferPolys
+    //'Okanagan Lake 2013 Imagery': OKimagery2013,
+    //'Okanagan Lake 2017 Imagery': OKimagery2017
+  },
+    {
+    collapsed: false
+    }
+  ).addTo(map);
+
+  /*-----GEOJSON-----*/
+
+  /*-----SCALEBAR-----*/
+  var scaleControl = L.control.scale(
+    {
+    imperial: false
+    }
+  ).addTo(map);
+
+});
