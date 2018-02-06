b5bfb638e17fcc0ea14fbe3718394d540f24f703
IanLaingBCGov <Ian.Laing@gov.bc.ca>
2018-02-06 10:55:27 -0800

    Aquifer WFS testing
    Aquifer WFS testing

--- js/gw_wells.js
+++ js/gw_wells.js
@@ -20,6 +20,43 @@ $( document ).ready(function() {
     minZoom: 1
   }).setView([49.6, -119.5], 9);
 
+  var aquiferURL = "https://openmaps.gov.bc.ca/geo/pub/WHSE_WATER_MANAGEMENT.GW_AQUIFERS_CLASSIFICATION_SVW/ows"
+
+  var defaultParameters = {
+    service: 'WFS',
+    version: '2.0',
+    request: 'GetFeature',
+    //typeName: 'Ground Water Aquifers',
+    outputFormat: 'text/javascript',
+    format_options: 'callback:getJson',
+    SrsName: 'EPSG:4326'
+  };
+
+  var parameters = L.Util.extend(defaultParameters);
+  var URL = aquiferURL + L.Util.getParamString(parameters);
+
+  var WFSLayer = null;
+  var ajax = $.ajax({
+    url: URL,
+    dataType: 'jsonp',
+    jsonpCallback: 'getJson',
+    success: function(response) {
+      WFSLayer = L.geoJson(response, {
+        style: function (feature) {
+          return{
+            stroke: false,
+            fillColor: 'FFFFFF',
+            fillOpacity: 0
+          };
+        },
+        onEachFeature: function (feature, layer) {
+          popupOptions = {maxWidth: 200};
+          layer.bindPopup(feature.properties.AQ_TAG);
+        }
+      }).addTo(map)
+    }
+  }) ;
+
 /*-----BASE MAPS-----*/
 
   var provRoadsWM = new L.tileLayer.wms("http://maps.gov.bc.ca/arcserver/services/province/roads_wm/MapServer/WMSServer", {
@@ -70,6 +107,7 @@ $( document ).ready(function() {
   */
 
   /*-----AQUIFERS POLY-----*/
+  /*
   var aquiferPolys = new L.tileLayer.wms("https://openmaps.gov.bc.ca/geo/pub/WHSE_WATER_MANAGEMENT.GW_AQUIFERS_CLASSIFICATION_SVW/ows", {
         layers: 'pub:WHSE_WATER_MANAGEMENT.GW_AQUIFERS_CLASSIFICATION_SVW',
         format: 'image/png',
@@ -78,31 +116,19 @@ $( document ).ready(function() {
         crs:crs84,
         styles: 'Aquifers_BC_Outlined'
   });
+  */
 
   /*-----GROUNDWATER WELLS-----*/
 
-  var gwWells = new L.tileLayer.wms("https://openmaps.gov.bc.ca/geo/pub/WHSE_WATER_MANAGEMENT.GW_AQUIFERS_CLASSIFICATION_SVW/ows?SERVICE=WMS&", {
+  var gwWells = new L.tileLayer.wms("https://openmaps.gov.bc.ca/geo/pub/WHSE_WATER_MANAGEMENT.GW_WATER_WELLS_WRBC_SVW/ows", {
         layers: 'pub:WHSE_WATER_MANAGEMENT.GW_WATER_WELLS_WRBC_SVW',
         format: 'image/png',
         version: '1.3.0',
-        transparent: false,
+        transparent: true,
         attribution: "© 2013-2018 GeoBC, DataBC, The Province of British Columbia",
         crs:crs84,
-        styles: 'Provincial_Groundwater_Observation_Wells_Active'
-  }).addTo(map);
-
-
-  /*-----POINTS OF DIVERSION-----*/
-  /*
-  var pointsOfDiversion = new L.tileLayer.wms("https://openmaps.gov.bc.ca/geo/pub/WHSE_WATER_MANAGEMENT.GW_AQUIFERS_CLASSIFICATION_SVW/ows", {
-        layers: 'pub:WHSE_WATER_MANAGEMENT.GW_WATER_WELLS_WRBC_SVW',
-        format: 'image/png',
-        transparent: true,
-        attribution: "© 2013-2016 GeoBC, DataBC, The Province of British Columbia",
-        crs:crs84,
-        styles: 'Points_of_Diversion'
-  }).addTo(map);
-  */
+        styles: 'Provincial_Groundwater_Observation_Wells_Active' //'Water_Wells_All'
+  });//.addTo(map);
 
 
   /*-----Layer Control-----*/
@@ -113,8 +139,8 @@ $( document ).ready(function() {
     //'SPOT Imagery': SPOTimagery
     },
     {
-    'Groundwater Wells (Scale Dependent)': gwWells,
-    'Aquifers (Scale Dependent)': aquiferPolys
+    //'Groundwater Wells (Scale Dependent)': gwWells,
+    //'Aquifers (Scale Dependent)': aquiferPolys
     //'Okanagan Lake 2013 Imagery': OKimagery2013,
     //'Okanagan Lake 2017 Imagery': OKimagery2017
   },
