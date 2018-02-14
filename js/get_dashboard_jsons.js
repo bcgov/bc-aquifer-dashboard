1d6fa22a78d897c361a020d90f169a0fb70b1c57
IanLaingBCGov <Ian.Laing@gov.bc.ca>
2018-02-14 12:38:56 -0800

    Added more general get_dashboard_jsons.js
    Added get_dashboard_jsons.js to retrieve dashboard data: aquifers, regions, precincts, wells
    
    Moved (archived) get_aquifer_json.js to testing

---
+++ js/get_dashboard_jsons.js
@@ -0,0 +1,67 @@
+﻿/* -----------------------------------------------------------------------------------
+   get_dashboard_jsons.js
+   Retrieves WFS (as JSONs) for the Aquifer Dashboard: makes various data available for 
+   different features of the dashboard (e.g., maps, graphs, widgets)
+   Developed by GeoBC
+   (c) 2018 GeoBC | http://www.geobc.gov.bc.ca
+   ----------------------------------------------------------------------------------- */
+
+$.urlParam = function(name) {
+  var results = new RegExp('[\\?&]' + name + '=([^&#]*)')
+      .exec(window.location.href);
+  if (!results) {
+    return 0;
+  }
+  return results[1] || 0;
+};
+
+//$( document ).ready(function() {
+
+//aquifer geoJson global
+var aquiferJson = {};
+
+//call this to load data into aquiferJson
+function getAquiferJSON(){
+  
+  
+  /*-----AQUIFER WFS-----*/
+  // https://gis.stackexchange.com/questions/64406/getting-wfs-data-from-geoserver-into-leaflet
+  var aquiferURL = "https://openmaps.gov.bc.ca/geo/pub/WHSE_WATER_MANAGEMENT.GW_AQUIFERS_CLASSIFICATION_SVW/ows"
+
+  var defaultParameters = {
+    service: 'WFS',
+    version: '2.0',
+    request: 'GetFeature',
+    typeName: 'WHSE_WATER_MANAGEMENT.GW_AQUIFERS_CLASSIFICATION_SVW',
+    outputFormat: 'text/javascript', //or application/json (but won't work w/ ajax dataType=jsonp )
+    format_options: 'callback:getJson',
+    SrsName: 'EPSG:4326',
+    //exclude 'GEOMETRY' from list below if spatial not required
+    propertyName: ['AQ_TAG', 'AQNAME', 'AQUIFER_MATERIALS', 'PRODUCTIVITY',
+    'VULNERABILITY', 'DEMAND', 'AQUIFER_CLASSIFICATION', 'AQUIFER_NAME',
+    'AQUIFER_RANKING_VALUE', 'DESCRIPTIVE_LOCATION', 'LITHO_STRATOGRAPHIC_UNIT',
+    'QUALITY_CONCERNS', 'AQUIFER_DESCRIPTION_RPT_URL', 'AQUIFER_STATISTICS_RPT_URL',
+    'AQUIFER_SUBTYPE_CODE', 'QUANTITY_CONCERNS', 'SIZE_KM2', 'TYPE_OF_WATER_USE',
+    'PRODUCTIVITY_CODE', 'DEMAND_CODE', 'VULNERABILITY_CODE', 'CLASSIFICATION_CODE',
+    'GEOMETRY', 'FEATURE_AREA_SQM']
+  };
+
+  var parameters = L.Util.extend(defaultParameters);
+  var URL = aquiferURL + L.Util.getParamString(parameters);
+
+  var WFSLayer = null;
+  //ajax (asynchronous HTTP) request https://www.sitepoint.com/ajaxjquery-getjson-simple-example/
+  var ajax = $.ajax({
+    url: URL,
+    dataType: 'jsonp',
+    jsonpCallback: 'getJson',
+    success: function(response) {
+      console.log('executed wfs request');
+    }
+  });
+}
+//this is a callback function to be run when JSON is returned by wfs call
+var getJson = function (response){
+  console.log('getJson callback function');
+  aquiferJson = response;
+};
