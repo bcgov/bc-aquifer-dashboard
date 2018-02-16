2242d71198f3432e452d77c3239af4f5a47f4ba5
IanLaingBCGov <Ian.Laing@gov.bc.ca>
2018-02-16 11:52:45 -0800

    Generic function to retrieve wfs layers
    Created a general function to retrieve the various wfs layers

--- js/get_dashboard_jsons.js
+++ js/get_dashboard_jsons.js
@@ -1,5 +1,5 @@
-﻿/* -----------------------------------------------------------------------------------
-   get_dashboard_jsons.js
+/* -----------------------------------------------------------------------------------
+   get_dashboard_jsons_testing.js
 
    Retrieves WFS layers (as JSONs) for the Aquifer Dashboard: makes various data
    available for different features of the dashboard (e.g., maps, graphs, widgets)
@@ -17,61 +17,98 @@ $.urlParam = function(name) {
   return results[1] || 0;
 };
 
-//geoJson globals
+//aquifer globals
 var aquiferJson = {};
+var aquiferURL = "https://openmaps.gov.bc.ca/geo/pub/WHSE_WATER_MANAGEMENT.GW_AQUIFERS_CLASSIFICATION_SVW/ows";
+var aquiferTypeName = 'WHSE_WATER_MANAGEMENT.GW_AQUIFERS_CLASSIFICATION_SVW';
+var aquiferProperties = ['AQ_TAG', 'AQNAME', 'AQUIFER_MATERIALS', 'PRODUCTIVITY',
+'VULNERABILITY', 'DEMAND', 'AQUIFER_CLASSIFICATION', 'AQUIFER_NAME',
+'AQUIFER_RANKING_VALUE', 'DESCRIPTIVE_LOCATION', 'LITHO_STRATOGRAPHIC_UNIT',
+'QUALITY_CONCERNS', 'AQUIFER_DESCRIPTION_RPT_URL', 'AQUIFER_STATISTICS_RPT_URL',
+'AQUIFER_SUBTYPE_CODE', 'QUANTITY_CONCERNS', 'SIZE_KM2', 'TYPE_OF_WATER_USE',
+'PRODUCTIVITY_CODE', 'DEMAND_CODE', 'VULNERABILITY_CODE', 'CLASSIFICATION_CODE',
+'GEOMETRY', 'FEATURE_AREA_SQM'];
+var aquiferCallback = 'getJsonAquifer';
+
+//aquifer callback function run when JSON is returned by wfs call
+var getJsonAquifer = function (response){
+  console.log(aquiferCallback + ' callback function');
+  aquiferJson = response;
+  //populate search box
+  //makeFilterList();
+};
+
+//NRS regions globals
 var regionsJson = {};
+var regionsURL = "https://openmaps.gov.bc.ca/geo/pub/WHSE_ADMIN_BOUNDARIES.ADM_NR_REGIONS_SPG/ows";
+var regionsTypeName = 'WHSE_ADMIN_BOUNDARIES.ADM_NR_REGIONS_SPG';
+var regionsProperties = ['REGION_NAME', 'ORG_UNIT', 'ORG_UNIT_NAME',
+'FEATURE_CODE', 'FEATURE_NAME', 'SHAPE', 'FEATURE_AREA_SQM'];
+var regionsCallback = 'getJsonRegions';
+
+//NRS regions callback function run when JSON is returned by wfs call
+var getJsonRegions = function (response){
+  console.log(regionsCallback + ' callback function');
+  regionsJson = response;
+};
+
+//water precincts globals
 var precinctsJson = {};
-var allWellsJson = {};
+var precinctsURL = "https://openmaps.gov.bc.ca/geo/pub/WHSE_ADMIN_BOUNDARIES.LWADM_WATMGMT_PREC_AREA_SVW/ows";
+var precinctsTypeName = 'WHSE_ADMIN_BOUNDARIES.LWADM_WATMGMT_PREC_AREA_SVW';
+var precinctsProperties = ['WATMGMT_PRECINCT_AREA_ID', 'FEATURE_CODE',
+'PRECINCT_NAME', 'PRECINCT_ID', 'DISTRICT_NAME', 'FEATURE_AREA_SQM', 'GEOMETRY'];
+var precinctsCallback = 'getJsonPrecincts';
 
-//comment out below if you don't want to automatically retrieve Aquifer JSON on page load
-$( document ).ready( getAquiferJSON );
+//water precincts callback function run when JSON is returned by wfs call
+var getJsonPrecincts = function (response){
+  console.log(precinctsCallback + ' callback function');
+  precinctsJson = response;
+};
 
-//call this to load data into aquiferJson
-function getAquiferJSON(){
+//water districts globals
+var districtsJson = {};
+var districtsURL = "https://openmaps.gov.bc.ca/geo/pub/WHSE_ADMIN_BOUNDARIES.LWADM_WATMGMT_DIST_AREA_SVW/ows";
+var districtsTypeName = 'WHSE_ADMIN_BOUNDARIES.LWADM_WATMGMT_DIST_AREA_SVW';
+var districtsProperties = ['WATMGMT_DISTRICT_AREA_ID', 'FEATURE_CODE',
+'DISTRICT_ID', 'DISTRICT_NAME', 'FEATURE_AREA_SQM', 'GEOMETRY'];
+var districtsCallback = 'getJsonDistricts';
 
-  /*-----AQUIFER WFS-----*/
-  // https://gis.stackexchange.com/questions/64406/getting-wfs-data-from-geoserver-into-leaflet
-  var aquiferURL = "https://openmaps.gov.bc.ca/geo/pub/WHSE_WATER_MANAGEMENT.GW_AQUIFERS_CLASSIFICATION_SVW/ows"
+//water districts callback function run when JSON is returned by wfs call
+var getJsonDistricts = function (response){
+  console.log(districtsCallback + ' callback function');
+  districtsJson = response;
+};
 
+//$( document ).ready(function() {});
+//$( document ).ready(getWFSjson(aquiferURL, aquiferTypeName, aquiferProperties, aquiferCallback));
+
+//fetch WFS (json) from openmaps geoserver
+function getWFSjson(wfsURL, wfsTypeName, wfsProperties, wfsCallback) {
   var defaultParameters = {
     service: 'WFS',
     version: '2.0',
     request: 'GetFeature',
-    typeName: 'WHSE_WATER_MANAGEMENT.GW_AQUIFERS_CLASSIFICATION_SVW',
+    typeName: wfsTypeName,
     outputFormat: 'text/javascript', //or application/json (but won't work w/ ajax dataType=jsonp )
-    format_options: 'callback:getJson',
+    format_options: 'callback:' + wfsCallback,
     SrsName: 'EPSG:4326',
     //exclude 'GEOMETRY' from list below if spatial not required
-    propertyName: ['AQ_TAG', 'AQNAME', 'AQUIFER_MATERIALS', 'PRODUCTIVITY',
-    'VULNERABILITY', 'DEMAND', 'AQUIFER_CLASSIFICATION', 'AQUIFER_NAME',
-    'AQUIFER_RANKING_VALUE', 'DESCRIPTIVE_LOCATION', 'LITHO_STRATOGRAPHIC_UNIT',
-    'QUALITY_CONCERNS', 'AQUIFER_DESCRIPTION_RPT_URL', 'AQUIFER_STATISTICS_RPT_URL',
-    'AQUIFER_SUBTYPE_CODE', 'QUANTITY_CONCERNS', 'SIZE_KM2', 'TYPE_OF_WATER_USE',
-    'PRODUCTIVITY_CODE', 'DEMAND_CODE', 'VULNERABILITY_CODE', 'CLASSIFICATION_CODE',
-    'FEATURE_AREA_SQM'] //'GEOMETRY'
+    propertyName: wfsProperties
   };
 
   var parameters = L.Util.extend(defaultParameters);
-  var URL = aquiferURL + L.Util.getParamString(parameters);
+  var URL = wfsURL + L.Util.getParamString(parameters);
 
-  var WFSLayer = null;
   //map.spin(true);
   //ajax (asynchronous HTTP) request https://www.sitepoint.com/ajaxjquery-getjson-simple-example/
   var ajax = $.ajax({
     url: URL,
     dataType: 'jsonp',
-    jsonpCallback: 'getJson',
+    jsonpCallback: wfsCallback,
     success: function(response) {
       console.log('executed wfs request');
       //map.spin(false);
     }
   });
 }
-
-//this is a callback function to be run when JSON is returned by wfs call
-var getJson = function (response){
-  console.log('getJson callback function');
-  aquiferJson = response;
-  //populate search box
-  //makeFilterList();
-};
