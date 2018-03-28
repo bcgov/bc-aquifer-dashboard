2d1856e0de17e076fdc984ec00a237579b7675a4
IanLaingBCGov <Ian.Laing@gov.bc.ca>
2018-03-28 11:07:31 -0700

    More CSV
    GWL_monthly_csv, GW_Well_Attributes_csv

---
+++ testing/get_CSV_data.js
@@ -0,0 +1,44 @@
+//Object containing databc datastore resource_id items
+var resourceIDs = {
+    'GWL_monthly_csv': 'c8c3c750-bf4d-4213-be58-42c6f42510e8', //circa 2014
+    'GW_Well_Attributes_csv': 'a8933793-eadb-4a9c-992c-da4f6ac8ca51', //circa 2014
+    //'ObservationWellDataAll_DailyMean_csv': 'caa18e44-c1a3-490f-a467-f2352bd8d382', // not in datastore
+    //'ObservationWellDataPast1years_Hourly_csv': 'a77e0aea-1216-470a-a00f-1f75e3c8432a' // not in datastore
+};
+
+//var resourceItem = GWL_monthly_csv;
+var datastoreURL = 'https://catalogue.data.gov.bc.ca/api/action/datastore_search';
+var csvData = {};
+var queryFilter = null; //'2014'; //'Observation Well #185';
+
+function loadCsv(resourceItem, callback) {
+    //load csv by resouce_id from data catalogue-datastore api
+    var data = {
+        resource_id: resourceItem, // the resource id from databc datastore
+        limit: 100000, // get max n results
+        q: queryFilter//query
+        };
+    return request = $.ajax({
+        url: datastoreURL,
+        data: data,
+        dataType: 'json',
+        success: function(response) {
+            //console.log(response);
+            return callback(response);
+        },
+        error: function(response) {
+            console.log(response.responseJSON.error);
+        }
+    });
+}
+
+//CSV callback function run when JSON is returned by loadCsv
+//https://github.com/maxogden/art-of-node#callbacks
+var csvCallback = function (response){
+    console.log('CSV callback function');
+    csvData = response.result.records;
+    console.log(csvData);
+};
+
+//load CSV from databc datastore via resource_id
+loadCsv(resourceIDs['GW_Well_Attributes_csv'], csvCallback);
